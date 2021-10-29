cd09b4cba057e98db180d92ef27d1a629937ea54
Genesis <genesisybenitez1@gmail.com>
2021-10-28 23:09:11 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+//logical operators
+
+let louie = {
+    breed: "poodle",
+    age: 2
+}
+
+let mona = {
+    breed: "german shephard",
+    age: 2
+}
+
+if(louie.breed === mona.breed || louie.age === mona.age){
+console.log("equal");
+}else{
+    console.log("not equal");
+}
+
+
+//equality
+// let a = 2;
+// let b = "2";
+
+// console.log(a === b);
+
+// let dog = {
+//     firstName: "louie"
+// }
+
+// console.log(dog["firstName"]);
+
+//comparison operators
+
+let x = 1;
+let y = "1";
+
+if(x !== y){
+    console.log("Not equal");
+}else{
+    console.log("equal");
+}
+
+// console.log(x !== y);
